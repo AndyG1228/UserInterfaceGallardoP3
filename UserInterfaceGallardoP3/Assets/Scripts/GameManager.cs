9076692d44018b15cf7aa371a471d4defb43aafb
AndyG1228 <606925@student.dusd.net>
2023-04-18 11:42:04 -0700

    Working on 5.3
    Game over function is not working as expected, collider triggers are the culprit.

---
+++ UserInterfaceGallardoP3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    private float spawnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int sscoreToAdd)
+    {
+        score += sscoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+}
